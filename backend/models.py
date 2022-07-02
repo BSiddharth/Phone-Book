a7657013d2693ff707b0286c930d7682f189d642
Siddharth Bisht <siddharthbisht99@gmail.com>
2022-07-02 22:31:35 +0530

    contact model added

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    number = Column(String, index=True)
